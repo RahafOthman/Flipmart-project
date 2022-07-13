458418dbf51d7e92fbb04579afe28e03aa9a0d6e
Aamer-Qanadilo <aamer_qanadilo@hotmail.com>
2022-07-13 10:58:30 +0300

    Added an alert in the main page

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     setTimeout(()=>{
-        $('body').css("overflow","auto");
-        $('.loader > div').fadeOut(1200, function(){
-            $('.loader').fadeOut(2000, function(){
+        $('.loader > div').fadeOut(1200, () => {
+            $('.loader').fadeOut(2000, () => {
                 $('div').remove('.loader');
+                $('body').css("overflow","auto");
+                if(location.href.includes("index.html")){
+                    Swal.fire({
+                        icon: 'warning',
+                        title: "You can login as admin to update the product's list\n\nEmail: admin@gmail.com\n\nPassword: admin",
+                        showConfirmButton: false,
+                        timer: 7500
+                    });
+                }
             }); 
         });
     }, 2000)
